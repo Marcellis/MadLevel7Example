366f45b8a03b5ed99bdd2e8f87f6f8430364ea91
marcomarcellis <m.m.c.m.marcellis@hva.nl>
2022-05-30 09:49:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/madlevel7example/HomeFragment.kt
@@ -0,0 +1,55 @@
+package com.example.madlevel7example
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.madlevel7example.databinding.FragmentHomeBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: QuizViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        //always retrieve quiz  when screen is shown
+        viewModel.getQuiz()
+
+        binding.btCreateQuiz.setOnClickListener {
+            navController.navigate(R.id.action_homeFragment_to_createQuizFragment)
+        }
+
+        viewModel.quiz.observe(viewLifecycleOwner, {
+            //make button visible and clickable
+            if (!it.answer.isBlank() || !it.answer.isBlank()) {
+                binding.btStartQuiz.alpha = 1.0f
+                binding.btStartQuiz.isClickable = true
+
+                binding.btStartQuiz.setOnClickListener {
+                    navController.navigate(R.id.action_homeFragment_to_quizFragment)
+                }
+            }
+        })
+
+    }
+}
